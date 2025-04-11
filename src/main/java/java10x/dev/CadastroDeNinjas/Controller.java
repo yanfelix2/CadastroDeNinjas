8b94bcfc780e539bca559771aebd0bdc6ca34aa6
Yan <yanfelixpimentell@gmail.com>
2025-04-10 22:20:27 -0300

    commit 10/04/25

---
+++ src/main/java/java10x/dev/CadastroDeNinjas/Controller.java
@@ -0,0 +1,19 @@
+package java10x.dev.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Fala pro java que isto é um controller.
+@RequestMapping // Para colocar todas as rotas no mesmo lugar.
+
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é a minha primeira mensagem nessa rota";
+    }
+
+
+
+}
