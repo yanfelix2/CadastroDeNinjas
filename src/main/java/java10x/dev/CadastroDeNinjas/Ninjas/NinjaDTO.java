9fbba6404234f0a865ad89470753781ebae81dc7
Yan <yanfelixpimentell@gmail.com>
2025-04-25 21:21:58 -0300

    Chore: Inclui a versao do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x dto

--- src/main/java/java10x/dev/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/java10x/dev/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,100 @@
 package java10x.dev.CadastroDeNinjas.Ninjas;
 
+import jakarta.persistence.*;
+import java10x.dev.CadastroDeNinjas.Missoes.MissoesModel;
+
 public class NinjaDTO {
+
+    private Long id;
+
+
+    private String nome;
+
+
+    private String email;
+
+
+    private String imgUrl;
+
+
+    private int idade;
+
+    private String rank;
+
+    private MissoesModel missoes;
+
+
+
+    public NinjaDTO() {
+    }
+
+    public NinjaDTO(Long id, String nome, String email, String imgUrl, int idade, MissoesModel missoes, String rank) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.imgUrl = imgUrl;
+        this.idade = idade;
+        this.missoes = missoes;
+        this.rank = rank;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public MissoesModel getMissoes() {
+        return missoes;
+    }
+
+    public void setMissoes(MissoesModel missoes) {
+        this.missoes = missoes;
+    }
+
+    public String getRank(){
+        return rank;
+    }
+
+    public void setRank(String rank){
+        this.rank = rank;
+    }
+
+
+
 }
+
